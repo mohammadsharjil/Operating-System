036d382d4143715abd33a2fb1052e068753dea73
Muhammad Sharjil <mohammads744@gmail.com>
2019-10-29 14:04:24 +0500

    File System Watcher

---
+++ Project(FileSystemWatcher)/Project(FileSystemWatcher)/MainForm.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Project_FileSystemWatcher_
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 100);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "File Filter";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Folder Path";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(14, 140);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(219, 21);
+            this.textBox2.TabIndex = 17;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(14, 54);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(412, 21);
+            this.textBox1.TabIndex = 16;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(485, 128);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 42);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Start Monitoring....";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(516, 44);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 40);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Browse ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(169, 528);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 40);
+            this.button3.TabIndex = 21;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(404, 528);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 40);
+            this.button4.TabIndex = 22;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // MainForm
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(684, 595);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Sitka Small", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.Text = "FileSystemWatcher";
+            this.TransparencyKey = System.Drawing.Color.DarkRed;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Project(FileSystemWatcher)/Project(FileSystemWatcher)/MainForm.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Media;
+
+namespace Project_FileSystemWatcher_
+{
+    public partial class MainForm : Form
+    {
+        public ListBox listBox;
+        public const String startMonitoring = "Start Monitoring…";
+        public const String stopMonitoring = "Stop Monitoring…";    
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            //Create a listBox to show activities of all Events.
+
+            try
+            {
+                listBox = new ListBox();
+                listBox.FormattingEnabled = true;
+                listBox.Location = new System.Drawing.Point(15, 201);
+                listBox.Name = "listBox";
+                listBox.Size = new System.Drawing.Size(651, 308);
+                listBox.TabIndex = 2;
+                this.Controls.Add(listBox);
+            }
+
+            catch
+            {
+                MessageBox.Show("Error, Something is Wrong!");
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Create FolderBrowserDialog object.
+
+                FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+                // Show a button to create a new folder.
+
+                Console.Beep();
+                folderBrowserDialog.ShowNewFolderButton = true;
+                DialogResult dialogResult = folderBrowserDialog.ShowDialog();
+
+                // Get selected path from FolderBrowserDialog control.
+
+                if (dialogResult == DialogResult.OK)
+                {
+                    Console.Beep();
+                    textBox1.Text = folderBrowserDialog.SelectedPath;
+                    Environment.SpecialFolder root = folderBrowserDialog.RootFolder;
+
+                }
+            }
+
+            catch
+            {
+                MessageBox.Show("Error, Something is Wrong!");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Create a new FileSystemWatcher object.
+
+            FileSystemWatcher fsWatcher = new FileSystemWatcher();
+
+            try
+               {
+
+            switch (button2.Text)
+            {
+                // Start Monitoring…
+                   
+                case startMonitoring:
+                  
+                        if (!textBox1.Text.Equals(String.Empty))
+                        {
+                            Console.Beep();
+                            listBox.Items.Add("Started File System Watcher Service…");
+                            fsWatcher.Path = textBox1.Text;
+
+                            // Set Filter.
+
+                            fsWatcher.Filter = (textBox2.Text.Equals(String.Empty)) ? "*.*" : textBox2.Text;
+
+                            // Monitor files and subdirectories.
+
+                            fsWatcher.IncludeSubdirectories = true;
+
+                            // Monitor all changes specified in the NotifyFilters.
+
+                            fsWatcher.NotifyFilter = NotifyFilters.Attributes |
+                                                     NotifyFilters.CreationTime |
+                                                     NotifyFilters.DirectoryName |
+                                                     NotifyFilters.FileName |
+                                                     NotifyFilters.LastAccess |
+                                                     NotifyFilters.LastWrite |
+                                                     NotifyFilters.Security |
+                                                     NotifyFilters.Size;
+                            fsWatcher.EnableRaisingEvents = true;
+
+                            // Raise Event handlers.
+
+                            fsWatcher.Changed += new FileSystemEventHandler(OnChanged);
+                            fsWatcher.Created += new FileSystemEventHandler(OnCreated);
+                            fsWatcher.Deleted += new FileSystemEventHandler(OnDeleted);
+                            fsWatcher.Renamed += new RenamedEventHandler(OnRenamed);
+                            fsWatcher.Error += new ErrorEventHandler(OnError);
+                            button2.Text = stopMonitoring;
+                            textBox1.Enabled = false;
+                            textBox2.Enabled = false;
+                        }
+
+                        else
+                        {
+                            listBox.Items.Add("Please select folder to monitor….");
+                        }
+
+                        break;
+                  
+
+                // Stop Monitoring…
+
+                case stopMonitoring:
+
+                default:
+                    Console.Beep();
+                    fsWatcher.EnableRaisingEvents = false;
+                    fsWatcher = null;
+                    button2.Text = startMonitoring;
+                    textBox1.Enabled = true;
+                    textBox2.Enabled = true;
+                    listBox.Items.Add("Stopped FileSystemWatcher Service…");
+                    break;                  
+             }
+        }
+
+             catch
+             {
+                 MessageBox.Show("Error, Something is Wrong!");
+             }
+
+        }
+
+        // FileSystemWatcher – OnCreated Event Handler
+
+        public void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            // Add event details in listbox.
+
+            this.Invoke((MethodInvoker)delegate { listBox.Items.Add(String.Format("Path : {0} || Action : {1}", e.FullPath, e.ChangeType)); });
+        }
+
+        // FileSystemWatcher – OnChanged Event Handler
+        
+        public void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            // Add event details in listbox.
+
+            this.Invoke((MethodInvoker)delegate { listBox.Items.Add(String.Format("Path : {0} || Action : {1}", e.FullPath, e.ChangeType)); });
+        }
+
+        // FileSystemWatcher – OnRenamed Event Handler
+        
+        public void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            // Add event details in listbox.
+
+            this.Invoke((MethodInvoker)delegate { listBox.Items.Add(String.Format("Path : {0}|| Action : {1} to {2}", e.FullPath, e.ChangeType, e.Name)); });
+        }
+
+        // FileSystemWatcher – OnDeleted Event Handler
+        
+        public void OnDeleted(object sender, FileSystemEventArgs e)
+        {
+            // Add event details in listbox.
+
+            this.Invoke((MethodInvoker)delegate { listBox.Items.Add(String.Format("Path : {0} || Action : {1}", e.FullPath, e.ChangeType)); });
+        }
+
+        // FileSystemWatcher – OnError Event Handler
+        
+        public void OnError(object sender, ErrorEventArgs e)
+        {
+            // Add event details in listbox.
+
+            this.Invoke((MethodInvoker)delegate { listBox.Items.Add(String.Format("Error : {0}", e.GetException().Message)); });
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                SoundPlayer sd = new SoundPlayer("t1.wav");
+                sd.Play();
+            }
+            catch
+            {
+                MessageBox.Show("Error, Something is Wrong!");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SoundPlayer sd1 = new SoundPlayer("t2.wav");
+                sd1.Play();              
+                Application.Exit();                
+            }
+            catch
+            {
+                MessageBox.Show("Error, Something is Wrong!");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Console.Beep();
+                this.Hide();
+                MainForm obj = new MainForm();
+                obj.Show();
+            }
+            catch
+            {
+                MessageBox.Show("Error, Something is Wrong!");
+            }
+        }
+    }
+}
